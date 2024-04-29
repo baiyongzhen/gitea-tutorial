3404cbac67e222de152969f59ee19d960a7de463
Baek.yj <dudwls96@gmail.com>
2024-04-29 22:49:29 +0900

    install argocd in k3s

---
+++ scripts/install_argocd_on_k3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Install k3s
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=" - write-kubeconfig=$HOME/local-cluster.config - write-kubeconfig-mode=644" sh -
+systemctl status k3s
+
+sudo chmod 766 /etc/rancher/k3s/k3s.yaml
+kubectl get ns
+
+# Install ArgoCD
+wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -O install.yaml
+kubectl create ns argocd
+kubectl apply -n argocd -f install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+#kubectl get all -n argocd
+kubectl get svc -n argocd
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
